59f17b335a15f06e3568b1d715e2d568b1fef2e9
Sinan <sinancem9@gmail.com>
2020-10-21 15:09:55 +0200

    Fixed a typo

--- move_uses.sh
+++ move_uses.sh
@@ -4,16 +4,16 @@ mkdir -p data/eng/corpus1/uses
 mkdir -p data/eng/corpus2/uses
 
 mv uses/eng/corpus1/* data/eng/corpus1/uses/
-mv uses/eng/corpus1/* data/eng/corpus2/uses/
+mv uses/eng/corpus2/* data/eng/corpus2/uses/
 
 mkdir -p data/ger/corpus1/uses
 mkdir -p data/ger/corpus2/uses
 
 mv uses/ger/corpus1/* data/ger/corpus1/uses/
-mv uses/ger/corpus1/* data/ger/corpus2/uses/
+mv uses/ger/corpus2/* data/ger/corpus2/uses/
 
 mkdir -p data/swe/corpus1/uses
 mkdir -p data/swe/corpus2/uses
 
 mv uses/swe/corpus1/* data/swe/corpus1/uses/
-mv uses/swe/corpus1/* data/swe/corpus2/uses/
+mv uses/swe/corpus2/* data/swe/corpus2/uses/
