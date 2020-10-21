35bb3936d8a6bc0fee6fcd05e0e84ebcd30dfb36
se1nan <sinancem9@gmail.com>
2020-10-21 11:25:38 +0200

    updated bash scripts

--- scripts/run_bert.sh
+++ scripts/run_bert.sh
@@ -3,14 +3,12 @@ language=$1
 outdir=output/bert/$language
 resdir=results/bert/$language
 
-
 # Generate uses 
 mkdir -p ${outdir}/uses_corpus1
 mkdir -p ${outdir}/uses_corpus2
 
 python modules/extract_uses.py data/${language}/corpus1/lemma/*.txt.gz data/${language}/corpus1/token/*.txt.gz data/${language}/targets.txt ${outdir}/uses_corpus1/ ${language}
 python modules/extract_uses.py data/${language}/corpus2/lemma/*.txt.gz data/${language}/corpus2/token/*.txt.gz data/${language}/targets.txt ${outdir}/uses_corpus2/ ${language}
-
 
 # Compute vectors with bert, compute APD and COS
 mkdir -p ${outdir}/vectors_corpus1
@@ -31,11 +29,9 @@ do
 
 done
 
-
 #Compute Spearman 
-python modules/spr.py data/${language}/truth/graded.txt ${resdir}/apd.txt graded apd 1 1 >> ${resdir}/spr_apd.txt
-python modules/spr.py data/${language}/truth/graded.txt ${resdir}/cos.txt graded cos 1 1 >> ${resdir}/spr_cos.txt
-
+python modules/spr.py data/${language}/truth/graded.txt ${resdir}/apd.txt 1 1 >> ${resdir}/spr_apd.txt
+python modules/spr.py data/${language}/truth/graded.txt ${resdir}/cos.txt 1 1 >> ${resdir}/spr_cos.txt
 
 # Clean up directory 
 rm -r output/bert/${language}
