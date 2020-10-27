38fb0bae2f2d6501c023fda0b7ea849cc699d8e0
Sinan Kurtyigit <sinancem9@gmail.com>
2020-10-27 11:11:42 +0100

    One script for everything sgns+op related

--- scripts/run_sgns+op.sh
+++ scripts/run_sgns+op.sh
@@ -1,12 +1,95 @@
 #!/bin/bash
+name=$0
 language=$1
+window_size=$2
+dim=$3
+k=$4
+t=$5
+min_count1=$6
+min_count2=$7
+itera=$8
+
+function usage {
+    echo ""
+    echo "  Usage1: ${name} <language> <window_size> <dim> <k> <t> <min_count> <itera>" 
+    echo ""
+    echo "      <language>      = eng | ger | swe | lat"
+    echo "      <window_size>   = the linear distance of context words to consider in each direction"
+    echo "      <dim>           = dimensionality of embeddings"
+    echo "      <k>             = number of negative samples parameter (equivalent to shifting parameter for PPMI)"
+    echo "      <t>             = threshold for subsampling"
+    echo "      <min_count1>    = number of occurrences for a word to be included in the vocabulary (corpus1)"
+    echo "      <min_count2>    = number of occurrences for a word to be included in the vocabulary (corpus2)"
+    echo "      <itera>         = number of iterations"
+    echo ""
+    echo "  Usage2: ${name} <language>"
+    echo ""
+    echo "      <language>      = eng | ger | swe | lat"
+    echo ""
+    echo "  Note: Usage2 choses the best performing paramters from SemEval2020 and How low can you go"
+    echo ""
+}
+
+if [ $# -ne 7 ] && [ $# -ne 1 ]
+	then 
+		usage
+		exit 1
+fi
+
+if [[ ( $1 == "--help") ||  $1 == "-h" ]] 
+	then 
+		usage
+		exit 0
+fi
+
+if [ $# -eq 1 ]
+    then 
+        if [ $1 == "eng" ]
+            then 
+                window_size=10
+                dim=350
+                k=5
+                t=None
+                min_count1=4
+                min_count2=4
+                itera=30
+        elif [ $1 == "ger" ]
+            then
+                window_size=10
+                dim=300
+                k=5
+                t=None
+                min_count1=39
+                min_count2=39
+                itera=5
+        elif [ $1 == "swe" ]
+            then
+                window_size=10
+                dim=500
+                k=5
+                t=None
+                min_count1=42
+                min_count2=65
+                itera=5
+        elif [ $1 == "lat" ]
+            then
+                window_size=10
+                dim=10
+                k=5
+                t=None
+                min_count1=1
+                min_count2=6
+                itera=30
+        fi
+fi
+
 outdir=output/sgns/$language
 resdir=results/sgns/$language
 
 #generate matrices with sgns
 mkdir -p ${outdir}
-python type-based/sgns.py data/${language}/corpus1/lemma/*.txt.gz ${outdir}/mat1 5 25 1 1 1 1
-python type-based/sgns.py data/${language}/corpus2/lemma/*.txt.gz ${outdir}/mat2 5 25 1 1 1 1
+python type-based/sgns.py data/${language}/corpus1/uses/modified_corpus.txt.gz ${outdir}/mat1 ${window_size} ${dim} ${k} ${t} ${min_count1} ${itera}
+python type-based/sgns.py data/${language}/corpus2/uses/modified_corpus.txt.gz ${outdir}/mat2 ${window_size} ${dim} ${k} ${t} ${min_count2} ${itera}
 
 #length-normalize and mean-center
 python modules/center.py -l ${outdir}/mat1 ${outdir}/mat1c
