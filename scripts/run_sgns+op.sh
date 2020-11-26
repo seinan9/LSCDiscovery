a7c3ae15867850faddd8bb8179902f18ad733afa
se1nan <sinancem9@gmail.com>
2020-11-26 13:23:18 +0100

    Now runs everything on Python3.8

--- scripts/run_sgns+op.sh
+++ scripts/run_sgns+op.sh
@@ -93,22 +93,22 @@ resdir=results/$language/sgns/${identifier}
 #generate matrices with sgns
 mkdir -p ${outdir}
 
-python type-based/sgns.py data/${language}/corpus1/lemma/*.txt.gz ${outdir}/mat1 ${window_size} ${dim} ${k} ${t} ${min_count1} ${itera}
-python type-based/sgns.py data/${language}/corpus2/lemma/*.txt.gz ${outdir}/mat2 ${window_size} ${dim} ${k} ${t} ${min_count2} ${itera}
+python3.8 type-based/sgns.py data/${language}/corpus1/lemma/*.txt.gz ${outdir}/mat1 ${window_size} ${dim} ${k} ${t} ${min_count1} ${itera}
+python3.8 type-based/sgns.py data/${language}/corpus2/lemma/*.txt.gz ${outdir}/mat2 ${window_size} ${dim} ${k} ${t} ${min_count2} ${itera}
 
 #length-normalize and mean-center
-python modules/center.py -l ${outdir}/mat1 ${outdir}/mat1c
-python modules/center.py -l ${outdir}/mat2 ${outdir}/mat2c
+python3.8 modules/center.py -l ${outdir}/mat1 ${outdir}/mat1c
+python3.8 modules/center.py -l ${outdir}/mat2 ${outdir}/mat2c
 
 #align with OP
-python modules/map_embeddings.py --normalize unit center --init_identical --orthogonal ${outdir}/mat1c ${outdir}/mat2c ${outdir}/mat1ca ${outdir}/mat2ca
+python3.8 modules/map_embeddings.py --normalize unit center --init_identical --orthogonal ${outdir}/mat1c ${outdir}/mat2c ${outdir}/mat1ca ${outdir}/mat2ca
 
 #measure CD
 mkdir -p ${resdir}
-python modules/cd.py -f -d ${outdir}/mat1ca ${outdir}/mat2ca data/${language}/targets.txt ${resdir}/cd.txt
+python3.8 modules/cd.py -f -d ${outdir}/mat1ca ${outdir}/mat2ca data/${language}/targets.txt ${resdir}/cd.txt
 
 #evaluate with SPR
-python modules/spr.py data/${language}/truth/graded.txt ${resdir}/cd.txt 1 1 >> ${resdir}/spr.txt
+python3.8 modules/spr.py data/${language}/truth/graded.txt ${resdir}/cd.txt 1 1 >> ${resdir}/spr.txt
 
 #clean directory
 rm -r output/${language}/sgns/${identifier}
