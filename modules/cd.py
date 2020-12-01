a506744b790c3ae256cdb6a6a102362fc0af481a
se1nan <sinancem9@gmail.com>
2020-12-01 10:59:58 +0100

    cd.py can now compute cd for every word in the two matrices

--- modules/cd.py
+++ modules/cd.py
@@ -18,13 +18,15 @@ def main():
     args = docopt("""Compute cosine distance for targets in two matrices.
 
     Usage:
-        cd.py <path_matrix1> <path_matrix2> <path_target_words> <path_output>
+        cd.py [-f] <path_matrix1> <path_matrix2> <path_targets> <path_output>
 
         <path_matrix1>      = path to first matrix
         <path_matrix2>      = path to second matrix
-        <path_target_words> = path to file with tab-separated word pairs (single column if -d is set)
+        <path_targets>      = path to file with target words
         <path_output>       = output path for result file
 
+    Options:
+        -f, --full          compute cosine distance for every word in the two matrices
      Note:
          Important: spaces must be already aligned (columns in same order)! Targets in first/second column of testset are computed from matrix1/matrix2.
         
@@ -32,8 +34,10 @@ def main():
     
     path_matrix1 = args['<path_matrix1>']
     path_matrix2 = args['<path_matrix2>']
-    path_target_words = args['<path_target_words>']
+    path_targets = args['<path_targets>']
     path_output = args['<path_output>']
+
+    is_full = args['--full']
 
     logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
     logging.info(__file__.upper())
@@ -53,30 +57,38 @@ def main():
     row2id1 = space1.row2id
     matrix2 = space2.matrix
     row2id2 = space2.row2id
+
+    distances = {}
     
-    # Load targets
-    with open(path_target_words, 'r', encoding='utf-8') as f_in:
-        targets = [(line.strip().split('\t')[0],line.strip().split('\t')[0]) for line in f_in]
-        
-    scores = {}
-    for (t1, t2) in targets:
-        
-        # Get row vectors
-        try:
-            v1 = matrix1[row2id1[t1]].toarray().flatten()
-            v2 = matrix2[row2id2[t2]].toarray().flatten()
-        except KeyError:
-            scores[(t1, t2)] = 'nan'
-            continue
-        
-        # Compute cosine distance of vectors
-        distance = cosine_distance(v1, v2)
-        scores[(t1, t2)] = distance
-        
-    with open(path_output, 'w', encoding='utf-8') as f_out:
-        for (t1, t2) in targets:
-            f_out.write('\t'.join(('%s,%s' % (t1,t2), str(scores[(t1, t2)])+'\n')))
-                
+    # Compute cosine distances
+    if is_full:
+        for key in row2id1:
+            try:
+                vec1 = matrix1[row2id1[key]].toarray().flatten()
+                vec2 = matrix2[row2id2[key]].toarray().flatten()
+                cd = cosine_distance(vec1, vec2)
+                distances[key] = cd
+            except KeyError:
+                pass
+    else:
+        with open(path_targets, 'r', encoding='utf-8') as f:
+            targets = [line.strip() for line in f]
+
+        for word in targets:
+            try:
+                vec1 = matrix1[row2id1[word]].toarray().flatten()
+                vec2 = matrix2[row2id2[word]].toarray().flatten()
+                cd = cosine_distance(vec1, vec2)
+                distances[word] = cd
+            except KeyError:
+                distances[word] = 'nan'
+                continue
+    
+    # Write output
+    with open(path_output+'.csv', 'w', encoding='utf-8') as f:
+        for key, value in distances.items():
+            f.write(key + '\t' + str(value) + '\n')
+
     logging.info("--- %s seconds ---" % (time.time() - start_time))                   
     print("")
     
