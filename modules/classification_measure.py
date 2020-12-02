751920c1d243dcb246783ebe80bc7ce3a8ece7c9
se1nan <sinancem9@gmail.com>
2020-12-02 14:05:11 +0100

    Now computed via sklearn metrics

--- modules/classification_measure.py
+++ modules/classification_measure.py
@@ -5,6 +5,7 @@ import time
 
 from docopt import docopt
 import numpy as np 
+from sklearn import metrics
 
 def main():
     """Compute various classification measures.
@@ -16,7 +17,7 @@ def main():
     Usage:
         classification_measures.py <path_truth> <path_file> 
 
-        <path_truth>    = path to binary gold data (tab-separated)
+        <path_truth>    = path to binary gold preds (tab-separated)
         <path_file>     = path to file containing words and binary values (tab-separated)
 
     """)
@@ -28,55 +29,29 @@ def main():
     logging.info(__file__.upper())
     start_time = time.time()
 
-    truth = {}
+    truth = []
     with open(path_truth, 'r', encoding='utf-8') as f:
         reader = csv.reader(f, delimiter='\t')
         for row in reader:
-            truth[row[0].strip()] = int(row[1].strip())
+            truth.append(int(row[1]))
     
-    data = {}
+    preds = []
     with open(path_file, 'r', encoding='utf-8') as f:
         reader = csv.reader(f, delimiter='\t')
         for row in reader:
-            data[row[0].strip()] = int(row[1].strip())
+            preds.append(int(row[1]))
 
-    true_positives = 0
-    false_positives = 0
-    true_negatives = 0
-    false_negatives = 0
+    precision = metrics.precision_score(truth, preds, zero_division=0)
+    recall = metrics.recall_score(truth, preds, zero_division=0)
+    bal_acc = metrics.balanced_accuracy_score(truth, preds)
+    f1 = metrics.f1_score(truth, preds, zero_division=0)
+    f05 = metrics.fbeta_score(truth, preds, beta=0.5, zero_division=0)
 
-    for word, value in data.items():
-        if data[word] == 1:
-            if truth[word] == 1:
-                true_positives += 1
-            else:
-                false_positives += 1
-        else:
-            if truth[word] == 0:
-                true_negatives += 1
-            else:
-                false_negatives += 1
-    
-    try:
-        precision = true_positives / (true_positives + false_positives)
-        recall = true_positives / (true_positives + false_negatives)
-        tpr = true_positives / (true_positives + false_negatives)
-        tnr = true_negatives / (true_negatives + false_positives)
-        balanced_accuracy = (tpr +tnr) / 2
-        f1 = compute_f_measure(precision, recall, 1)
-        f05 = compute_f_measure(precision, recall, 0.5)
-    except ZeroDivisionError:
-        print("ZeroDivisionError")
-        sys.exit()
+    print('\t'.join((str(precision), str(recall), str(bal_acc), str(f1), str(f05))))
 
-    print('\t'.join((str(precision), str(recall), str(balanced_accuracy), str(f1), str(f05))))
 
     logging.info("--- %s seconds ---" % (time.time() - start_time))    
     print("")
-
-def compute_f_measure(precision, recall, beta):
-    f1_measure = (1 + beta**2) * ((precision * recall) / (beta**2 * (precision + recall)))
-    return f1_measure
 
 
 if __name__ == '__main__':
