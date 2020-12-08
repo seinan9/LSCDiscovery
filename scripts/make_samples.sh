e8128ddc83e3aa7a890fe4b69cc8471c21fbac98
se1nan <sinancem9@gmail.com>
2020-12-08 15:42:06 +0100

    auto. generate uses for samples

---
+++ scripts/make_samples.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+name=$0
+language=$1
+
+function usage {
+	echo "Generate samples and extract 100 uses from both time periods for every sample."
+    echo ""
+    echo "  Usage:" 
+    echo "      ${name} <language>"
+    echo ""
+    echo "      <language>  = eng | ger | swe"
+    echo ""
+}
+
+if [ $# -ne 1 ] 
+	then 
+		usage
+		exit 1
+fi
+
+if [[ ( $1 == "--help") ||  $1 == "-h" ]] 
+	then 
+		usage
+		exit 0
+fi
+
+
+mkdir -p data/${language}/samples
+mkdir -p sample_uses/${language}/corpus1
+mkdir -p sample_uses/${language}/corpus2
+
+# Generate frequency lists
+python3.8 measures/freqs.py data/${language}/corpus1_preprocessed/lemma/*txt.gz data/${language}/samples/freqs1.tsv
+python3.8 measures/freqs.py data/${language}/corpus2_preprocessed/lemma/*txt.gz data/${language}/samples/freqs2.tsv
+
+# Generate samples
+python3.8 modules/sample.py data/${language}/samples/freqs1.tsv data/${language}/samples/freqs2.tsv data/${language}/targets.txt data/${language}/samples/
+
+# Extract uses for samples
+python modules/extract_uses.py data/${language}/corpus1/lemma/*.txt.gz data/${language}/corpus1/token/*.txt.gz data/${language}/samples/samples.tsv sample_uses/${language}/corpus1/ ${language}
+python modules/extract_uses.py data/${language}/corpus2/lemma/*.txt.gz data/${language}/corpus2/token/*.txt.gz data/${language}/samples/samples.tsv sample_uses/${language}/corpus2/ ${language}
+
+
